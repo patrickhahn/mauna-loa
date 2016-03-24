5b9a4af5747fbdb846b6bb18a3650913836ef051
Patrick Hahn <phahn13@gmail.com>
2016-03-23 21:59:53 -0400

    Adding combined CO2 level data through February, and script to predict March/April with ARMA model

---
+++ arma_predict.r
@@ -0,0 +1,21 @@
+library("itsmr")
+
+# Load data from .tsm file that should be located in same
+# directory as this script
+script.dir <- dirname(sys.frame(1)$ofile)
+co2 <- scan(paste(script.dir, "/maunaloa.tsm", sep=""))
+
+plotc(co2)
+
+# estimate season & trend, then remove them
+xv = c("season", 12, "trend", 1)
+e = Resid(co2, xv)
+
+# This line was used to find ultimate p and q (p=5, q=3)
+# a = autofit(e, p=0:5, q=0:5)
+
+# Estimate ARMA model coefficients
+a = arma(e, p=5, q=3)
+
+# Forecast 2 months ahead (March and April)
+forecast(co2, xv, a, h=2, opt=2)
